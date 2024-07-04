6cd49b0671d2669980db68e82f6476711752af00
HADDADmed <115414476+HADDADmed@users.noreply.github.com>
2024-07-04 05:10:40 +0100

    implemented recycler tabs especiali the map

---
+++ app/(recycler)/_layout.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Drawer } from 'expo-router/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+const placeholderAvatar = 'https://via.placeholder.com/150'; // Placeholder avatar URL
+
+// Component for rendering avatar in header
+const HeaderAvatar = () => (
+  <TouchableOpacity style={styles.avatarContainer} onPress={() => {}}>
+    <Image source={{ uri: placeholderAvatar }} style={styles.avatar} />
+  </TouchableOpacity>
+);
+
+// Main component for RecyclerTabLayout
+export default function RecyclerTabLayout() {
+  return (
+    <Drawer
+      screenOptions={{
+        headerShown: true,
+        headerTitleAlign: 'center',
+        headerTitleStyle: { fontSize: 20, fontWeight: 'bold' },
+        headerRight: () => <HeaderAvatar />,
+      }}
+    >
+      <Drawer.Screen
+        name="map-recycler-page"
+ 
+        options={{
+          title: 'Map for Recycler',
+          drawerIcon: () => <Ionicons name="map" size={24} color="#3498db" />,
+        }}
+      />
+    </Drawer>
+  );
+}
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    overflow: 'hidden',
+    marginRight: 10,
+  },
+  avatar: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,28 @@
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Redirect } from 'expo-router';
 import Login from '@/components/login';
 
 export default function HomeScreen() {
   // Simulate user login state
-  const [user, setUser] = useState(true); // Assume user is logged in
+  const [user, setUser] = useState({
+    isConnected: true,
+    role: 'recycler'
+  }); // Assume user is logged in
 
-  if (user) {
+  // Redirect if user is not connected
+  if (!user.isConnected) {
+    return <Redirect href="/login" />;
+  }
+
+  // Redirect based on user role
+  if (user.role === 'recycler') {
+    return <Redirect href="/map-recycler-page" />;
+  } else if (user.role === 'collector') {
     return <Redirect href="/map-page" />;
   }
 
+  // Default rendering if none of the conditions are met
   return (
     <View style={styles.container}>
       <Text>Home</Text>
